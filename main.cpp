24e7f361f7c85a2896b7d22c21ae89782e771eb6
Your Name <you@example.com>
2024-03-14 12:23:26 +0200

    Example commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main() {
     std::array<int, 100> v{};
     int nr;
     std::cout << "Introduceți nr: ";
+
     /////////////////////////////////////////////////////////////////////////
     /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
     /// dați exemple de date de intrare folosind fișierul tastatura.txt
