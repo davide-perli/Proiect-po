d75d9899d44009bb54e39ac76877d396660a1af3
Davide <you@example.com>
2024-04-19 00:02:25 +0300

    Apelarea functiilor, getteri, setteri in main pentru cppcheck

--- main.cpp
+++ main.cpp
@@ -589,7 +589,7 @@ void salutare()
         std::cout << "=======================================================================================================================" << std::endl;
         std::cout << std::endl;
         Angajati a, a1, a2("Davide", "07243435345");
-        AngajatiExtins a3;
+        AngajatiExtins a3, angajat("John", "1234567890", 2000, 40);;
         std::cin >> a3;
         std::cout << std::endl;
         std::cout << a3;
@@ -600,6 +600,19 @@ void salutare()
         std::cout << std::endl;
         std::cout << "=======================================================================================================================" << std::endl;
         std::cout << std::endl;
+
+        //testare getteri si setteri
+        angajat.setSalariu(2500);
+        double salariu = angajat.getSalariu();
+        std::cout << "Salariu: " << salariu << std::endl;
+        std::cout << std::endl;
+        std::cout << "=======================================================================================================================" << std::endl;
+
+        angajat.setOreLucrate(45);
+        int oreLucrate = angajat.getOreLucrate();
+        std::cout << "Ore lucrate: " << oreLucrate << std::endl;
+        std::cout << std::endl;
+        std::cout << "=======================================================================================================================" << std::endl;
 
         //getter/setter
         std::cout << "Getter && Setter:" << std::endl;
