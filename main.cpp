203d439d5b25431c8735d6fb134984a16cf65656
Davide <you@example.com>
2024-04-19 21:15:18 +0300

    verificare functie void de salutare pusa in header

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-//#include <utility>
 #include <vector>
 #include <algorithm>
 #include <thread>
@@ -10,38 +9,7 @@
 #include "AngajatiExtins.h"
 #include "Manager.h"
 #include "listaComenzi.h"
-
-void salutare()
-{
-    std::cout << "========================================================================================================================" << std::endl;
-    std::cout << "\t\t\tBine ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "ati ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "venit ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "la ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "magazinul ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "de ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "inghetata ";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "Pufic";
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << "!" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    std::cout << std::endl;
-    std::cout << "========================================================================================================================" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-
-    for (int i = 0; i < 13; i++) {
-        std::cout << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    }
-
-}
+#include "salutare.h"
 
 int main()
 {
