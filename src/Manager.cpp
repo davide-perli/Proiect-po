3a41e62bd810421983a99cca13a399a9e177774c
Davide <you@example.com>
2024-04-19 17:09:20 +0300

    Eu renunt, la mine n-am erori si prostia aia de asan windows 2022 msvc 19.38 ma termina psihic

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -5,7 +5,7 @@
 
 #include "Manager.h"
 
-Manager::Manager() : Angajati(), salariuLunar(0) {}
+Manager::Manager() : salariuLunar(0) {}
 
 void Manager::setNumeManager(const std::string &nume) {
     Angajati::setNume(nume.c_str()); // Accesează membrul nume din clasa de bază Angajati, chipurile aici era problema de suprascriere
